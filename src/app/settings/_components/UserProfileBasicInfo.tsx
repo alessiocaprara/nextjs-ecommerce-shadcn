2b1f18ebd2665d8a1bbc06176d74d5c9ed2f0c16
Alessio <capraraalessio@gmail.com>
2023-11-01 18:53:55 +0100

    Text Item done

--- src/app/settings/_components/UserProfileBasicInfo.tsx
+++ src/app/settings/_components/UserProfileBasicInfo.tsx
@@ -1,6 +1,10 @@
+"use client"
+
 import profilePicPlaceholder from "@/assets/profile-pic-placeholder.png"
 import { User } from "@prisma/client"
+import { ChevronRight } from "lucide-react"
 import Image from "next/image"
+import { use, useState } from "react"
 import UserProfileDateItem from "./UserProfileDateItem"
 import UserProfileSelectItem from "./UserProfileSelectItem"
 import UserProfileTextItem from "./UserProfileTextItem"
@@ -12,33 +16,58 @@ const genders = [
 ]
 
 export default function UserProfileBasicInfo({ user }: { user: User }) {
+    const [usernameEditing, setUsernameEditing] = useState(false);
+    const [dobEditing, setDobEditing] = useState(false);
+    const [genderEditing, setGenderEditing] = useState(false);
+
     return (
-        <div className="flex flex-col border rounded-xl">
+        <>
+            {(!usernameEditing && !dobEditing && !genderEditing) &&
 
-            <div className="px-4 pt-4 pb-6">
-                <div className="text-lg font-medium">Basic info</div>
-                <div className="text-sm text-muted-foreground">{user.email}</div>
-            </div>
+                <div className="flex flex-col">
 
-            <div className="flex items-center py-3 px-4 hover:bg-muted hover:cursor-pointer">
-                <div className="flex align-top w-full">
-                    <div className="w-40 flex-shrink-0 text-sm font-medium leading-none">Profile picture</div>
-                    <div className="w-full pe-4 text-sm leading-none">A profile picture helps personalize your account</div>
+                    <div className="px-4 pb-6">
+                        <div className="text-lg font-medium">Basic info</div>
+                        <div className="text-sm text-muted-foreground">{user.email}</div>
+                    </div>
+
+                    <div className="flex items-center py-3 px-4 hover:bg-muted hover:cursor-pointer">
+                        <div className="flex align-top w-full">
+                            <div className="w-40 flex-shrink-0 text-sm font-medium leading-none">Profile picture</div>
+                            <div className="w-full pe-4 text-sm leading-none">A profile picture helps personalize your account</div>
+                        </div>
+                        <Image
+                            src={user.image || profilePicPlaceholder}
+                            style={{ objectFit: "cover" }}
+                            width={80}
+                            height={80}
+                            alt="preview image"
+                            className="rounded-full h-16 w-16 flex-shrink-0 border border-black"
+                        />
+                    </div>
+
+                    <div className="flex items-center py-4 px-4 hover:bg-muted hover:cursor-pointer" onClick={() => setUsernameEditing(true)}>
+                        <div className="flex align-top w-full">
+                            <div className="w-40 flex-shrink-0 text-sm font-medium leading-none">{"Username"}</div>
+                            <div className="w-full pe-4 text-sm leading-none">{user.username ?? ""}</div>
+                        </div>
+                        <ChevronRight className="h-6 w-6 flex-shrink-0 me-2" />
+                    </div>
+                    <UserProfileDateItem userId={user.id} label="Date of birth" fieldName="dob" value={user.dob} />
+                    <UserProfileSelectItem userId={user.id} label="Gender" fieldName="gender" possibileValues={genders} currentValue={user.gender} className="rounded-b-xl" />
+
                 </div>
-                <Image
-                    src={user.image || profilePicPlaceholder}
-                    style={{ objectFit: "cover" }}
-                    width={80}
-                    height={80}
-                    alt="preview image"
-                    className="rounded-full h-16 w-16 flex-shrink-0 border border-black"
+
+            }
+            {usernameEditing &&
+                <UserProfileTextItem
+                    userId={user.id}
+                    fieldName="username"
+                    label={"Username"}
+                    value={user.username}
+                    onClose={() => { setUsernameEditing(false) }}
                 />
-            </div>
-
-            <UserProfileTextItem userId={user.id} label="Username" fieldName="username" value={user.username} />
-            <UserProfileDateItem userId={user.id} label="Date of birth" fieldName="dob" value={user.dob} />
-            <UserProfileSelectItem userId={user.id} label="Gender" fieldName="gender" possibileValues={genders} currentValue={user.gender} className="rounded-b-xl" />
-
-        </div>
+            }
+        </>
     )
 }
--- src/app/settings/_components/UserProfileTextItem.tsx
+++ src/app/settings/_components/UserProfileTextItem.tsx
@@ -1,15 +1,14 @@
 "use client"
 
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog"
 import { Button } from "@/components/ui/button"
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { toast } from "@/components/ui/use-toast"
-import { cn } from "@/lib/utils"
 import { zodResolver } from "@hookform/resolvers/zod"
 import delay from "delay"
-import { ChevronRight, Loader2 } from "lucide-react"
-import { useEffect, useState, useTransition } from "react"
+import { ArrowLeft, Loader2 } from "lucide-react"
+import { useEffect, useTransition } from "react"
 import { useForm } from "react-hook-form"
 import * as z from "zod"
 import { updateRemoveUser, updateUser } from "../actions"
@@ -25,14 +24,14 @@ interface UserProfileTextItemProps {
     fieldName: string,
     label: string,
     value: string | null,
+    onClose: () => void,
     className?: string,
 }
 
-export default function UserProfileTextItem({ userId, fieldName, label, value, className }: UserProfileTextItemProps) {
+export default function UserProfileTextItem({ userId, fieldName, label, value, onClose, className }: UserProfileTextItemProps) {
 
     const [isPendingTransition1, startTransition1] = useTransition();
     const [isPendingTransition2, startTransition2] = useTransition();
-    const [open, setOpen] = useState(false);
 
     const form = useForm<userProfileTextItemValues>({
         resolver: zodResolver(userProfileTextItemSchema),
@@ -51,18 +50,11 @@ export default function UserProfileTextItem({ userId, fieldName, label, value, c
         await delay(500);
         startTransition1(async () => {
             try {
-                if (!form.formState.dirtyFields.userField) {
-                    setOpen(false);
-                    return
-                };
-
+                if (!form.formState.dirtyFields.userField) return;
                 const formData = new FormData();
                 formData.set(fieldName, data.userField.trim());
-
+                onClose();
                 await updateUser(userId, formData);
-
-                setOpen(false);
-
                 toast({
                     variant: "confirmation",
                     description: "Profile successfully updated."
@@ -77,29 +69,19 @@ export default function UserProfileTextItem({ userId, fieldName, label, value, c
     }
 
     return (
-        <Dialog
-            open={open}
-            onOpenChange={(open) => {
-                setOpen(open);
-                if (!open) form.reset();
-            }}
-        >
-            <DialogTrigger asChild>
-                <div className={cn("flex items-center py-4 px-4 hover:bg-muted hover:cursor-pointer", className)} >
-                    <div className="flex align-top w-full">
-                        <div className="w-40 flex-shrink-0 text-sm font-medium leading-none">{label}</div>
-                        <div className="w-full pe-4 text-sm leading-none">{value ? value : ""}</div>
+        <>
+            <div className="px-4 space-y-8">
+                <div className="flex pb-4">
+                    <div className="w-full flex flex-col">
+                        <div className="flex items-center text-lg font-medium">
+                            <ArrowLeft className="opacity-80 hover:cursor-pointer hover:bg-muted rounded-full p-1" size={30} onClick={() => onClose()} />
+                            Edit profile
+                        </div>
+                        <div className="text-sm text-muted-foreground">Make changes to your profile here. Click save when you are done.</div>
                     </div>
-                    <ChevronRight className="h-6 w-6 flex-shrink-0 me-2" />
                 </div>
-            </DialogTrigger>
-            <DialogContent className="sm:max-w-[425px]">
-                <DialogHeader>
-                    <DialogTitle>Edit profile</DialogTitle>
-                    <DialogDescription>Make changes to your profile here. Click save when you are done.</DialogDescription>
-                </DialogHeader>
                 <Form {...form}>
-                    <form id="this-form" onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 py-4" autoComplete="off">
+                    <form id="text-item-form" onSubmit={form.handleSubmit(onSubmit)} autoComplete="off">
                         <FormField
                             control={form.control}
                             name="userField"
@@ -107,7 +89,7 @@ export default function UserProfileTextItem({ userId, fieldName, label, value, c
                                 <FormItem>
                                     <FormLabel>{label}</FormLabel>
                                     <FormControl>
-                                        <Input {...field} />
+                                        <Input {...field} className="w-1/2" />
                                     </FormControl>
                                     <FormMessage />
                                 </FormItem>
@@ -115,46 +97,56 @@ export default function UserProfileTextItem({ userId, fieldName, label, value, c
                         />
                     </form>
                 </Form>
-                <DialogFooter>
-                    <Button
-                        variant="outline"
-                        disabled={(value === null) || isPendingTransition2}
-                        onClick={() => {
-
-
-
-
-                            startTransition2(async () => {
-                                try {
-                                    await delay(500);
-                                    await updateRemoveUser(userId, fieldName);
-                                    setOpen(false);
-                                    toast({
-                                        variant: "confirmation",
-                                        description: "Profile successfully updated."
-                                    })
-                                } catch (error) {
-                                    toast({
-                                        variant: "destructive",
-                                        description: (error as Error).message
-                                    })
-                                }
-                            })
-                        }}
-                    >
-                        Remove
-                        {isPendingTransition2 && <Loader2 className="ml-1 h-4 w-4 animate-spin" />}
-                    </Button>
+                <div className="space-x-3">
                     <Button
                         type="submit"
-                        form="this-form"
+                        form="text-item-form"
                         disabled={form.formState.isSubmitting || !form.formState.dirtyFields.userField || !form.watch("userField")}
                     >
                         Save changes
                         {form.formState.isSubmitting && <Loader2 className="ml-1 h-4 w-4 animate-spin" />}
                     </Button>
-                </DialogFooter>
-            </DialogContent>
-        </Dialog >
+                    <AlertDialog>
+                        <AlertDialogTrigger asChild>
+                            <Button variant="outline" disabled={value === null}>Remove</Button>
+                        </AlertDialogTrigger>
+                        <AlertDialogContent>
+                            <AlertDialogHeader className="mb-6">
+                                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                                <AlertDialogDescription>
+                                    This action cannot be undone.
+                                </AlertDialogDescription>
+                            </AlertDialogHeader>
+                            <AlertDialogFooter>
+                                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                                <AlertDialogAction
+                                    disabled={isPendingTransition2}
+                                    onClick={() => {
+                                        startTransition2(async () => {
+                                            try {
+                                                onClose();
+                                                await updateRemoveUser(userId, fieldName);
+                                                toast({
+                                                    variant: "confirmation",
+                                                    description: "Profile successfully updated."
+                                                })
+                                            } catch (error) {
+                                                toast({
+                                                    variant: "destructive",
+                                                    description: (error as Error).message
+                                                })
+                                            }
+                                        })
+                                    }}
+                                >
+                                    Continue
+                                    {isPendingTransition2 && <Loader2 className="ml-1 h-4 w-4 animate-spin" />}
+                                </AlertDialogAction>
+                            </AlertDialogFooter>
+                        </AlertDialogContent>
+                    </AlertDialog>
+                </div>
+            </div >
+        </>
     )
 }
